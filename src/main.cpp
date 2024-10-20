3392386f3ce0a22109e1fdc8ce56538db5e28f05
kodesun <168811279+kodesun@users.noreply.github.com>
2024-10-20 12:47:57 -0700

    removed: useless docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-/*
- * Black Jack game implementation directly through the CLI.
- * Created by @sudfish 
- * Start Date - 2024-05-27
- * End Date - 2024-05-31 
-*/
-
-
 #include "Game.hpp"
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     blackjack::Game game = blackjack::Game();
     game.Run();
     return 0;
